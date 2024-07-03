92c20aabf5a990f5ffc2bd2d72aae442ee0eaebd
Himanshu <xylogramori@gmail.com>
2024-07-04 03:44:22 +0530

    Upload Repo added

---
+++ src/app/teams/[teamId]/github-Repos/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Image from "next/image";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import AuthContext from "@/context/Authcontext";
+import { getResourcesByType } from "@/services/resourceServices";
+import UploadResource from "@/components/UploadResource/UploadResource";
+import DocumentResource from "@/components/DocumentResource/DocumentResource";
+import UploadRepo from "@/components/UploadRepo/UploadRepo";
+
+
+const TeamImagesPage = () => {
+  const params = useParams<any>();
+  const authContext = useContext(AuthContext);
+
+  const [error, setError] = useState("");
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(() => {
+    if (authContext?.user == null) {
+      authContext?.setUserUsingtokens();
+    }
+    const fetchImages = async () => {
+      try {
+        const documentData = await getResourcesByType(params?.teamId, "document");
+        setDocuments(documentData);
+      } catch (error: any) {
+        setError("error fetching documents");
+        console.log(error);
+      }
+    };
+    fetchImages();
+  }, []);
+
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName={`Teams /  MyTeam / documents`} />
+      {/* image upload */}
+      <UploadRepo/>
+      {/* image data */}
+      <div className="py-4">
+      <h2 className="text-2xl dark:text-white text-black font-bold mb-4">Github Repositories</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+        {documents.map((document:any) => (
+          <DocumentResource setDocuments={setDocuments} documents={document} document={document} key={document?._id} />
+        ))}
+      </div>
+    </div>
+      
+    </DefaultLayout>
+  );
+};
+
+export default TeamImagesPage;
