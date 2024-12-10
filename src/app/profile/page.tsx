f6f1fa74dc07f47ac0ac24bc4509b390bebd7b7d
Himanshu <xylogramori@gmail.com>
2024-12-10 11:05:30 +0530

    Socials

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import Image from "next/image";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
@@ -19,35 +19,31 @@ const Profile = () => {
 
   return (
     <DefaultLayout>
-      {show && (
-          <UpdateProfilePic setShow={setShow} type={type}/>
-        )}
+      {show && <UpdateProfilePic setShow={setShow} type={type} />}
 
       <div className="mx-auto max-w-242.5">
         <Breadcrumb pageName="Profile" />
-     
 
         <div className="overflow-hidden rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-          <div className="relative overflow-y-hidden  h-35 md:h-65">
+          <div className="relative h-35  overflow-y-hidden md:h-65">
             <Image
-              src={authContext?.user?.coverPicture || "/images/cover/cover-01.png"}
+              src={
+                authContext?.user?.coverPicture || "/images/cover/cover-01.png"
+              }
               alt="profile cover"
               className="h-full w-full rounded-tl-sm rounded-tr-sm object-cover object-center"
               width={970}
               height={260}
-              style={{objectFit: "cover"}}
-              
-              
+              style={{ objectFit: "cover" }}
             />
             <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
               <button
-                 onClick={() => {
+                onClick={() => {
                   setShow(!show);
-                  setType("cover")
+                  setType("cover");
                 }}
                 className="flex cursor-pointer items-center justify-center gap-2 rounded bg-primary px-2 py-1 text-sm font-medium text-white hover:bg-opacity-80 xsm:px-4"
               >
-               
                 <span>
                   <svg
                     className="fill-current"
@@ -82,20 +78,20 @@ const Profile = () => {
                   src={authContext?.user?.profilePicture || "/blackprofile.png"}
                   width={160}
                   height={160}
-                  className="rounded-full overflow-hidden"
+                  className="overflow-hidden rounded-full"
                   style={{
-                   objectFit:"fill"
+                    objectFit: "fill",
                   }}
                   alt="profile"
                 />
                 <button
-                    type="submit"
-                    title="editprofilepic"
-                     onClick={() => {
-                      setShow(!show);
-                      setType("profile")
-                    }}
-                  className="absolute z-999 bottom-0 right-0 flex h-8.5 w-8.5 cursor-pointer items-center justify-center rounded-full bg-primary text-white hover:bg-opacity-90 sm:bottom-2 sm:right-2"
+                  type="submit"
+                  title="editprofilepic"
+                  onClick={() => {
+                    setShow(!show);
+                    setType("profile");
+                  }}
+                  className="absolute bottom-0 right-0 z-999 flex h-8.5 w-8.5 cursor-pointer items-center justify-center rounded-full bg-primary text-white hover:bg-opacity-90 sm:bottom-2 sm:right-2"
                 >
                   <svg
                     className="fill-current"
@@ -118,19 +114,16 @@ const Profile = () => {
                       fill=""
                     />
                   </svg>
-      
                 </button>
-                    
-                 
-                
-
               </div>
             </div>
             <div className="mt-4">
               <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
                 {authContext?.user?.fullName}
               </h3>
-              <p className="font-medium">Username : {authContext?.user?.username}</p>
+              <p className="font-medium">
+                Username : {authContext?.user?.username}
+              </p>
               {/* <div className="mx-auto mb-5.5 mt-4.5 grid max-w-94 grid-cols-3 rounded-md border border-stroke py-2.5 shadow-1 dark:border-strokedark dark:bg-[#37404F]">
                 <div className="flex flex-col items-center justify-center gap-1 border-r border-stroke px-4 dark:border-strokedark xsm:flex-row">
                   <span className="font-semibold text-black dark:text-white">
@@ -154,11 +147,9 @@ const Profile = () => {
 
               <div className="mx-auto max-w-180">
                 <h4 className="font-semibold text-black dark:text-white">
-                {authContext?.user?.bio && "About Me"}
+                  {authContext?.user?.bio && "About Me"}
                 </h4>
-                <p className="mt-4.5">
-                  {authContext?.user?.bio}
-                </p>
+                <p className="mt-4.5">{authContext?.user?.bio}</p>
               </div>
 
               <div className="mt-6.5">
@@ -167,7 +158,8 @@ const Profile = () => {
                 </h4>
                 <div className="flex items-center justify-center gap-3.5">
                   <Link
-                    href="#"
+                    href={authContext?.user?.socials?.facebook || "#"}
+                    target="_blank"
                     className="hover:text-primary"
                     aria-label="social-icon"
                   >
@@ -192,39 +184,10 @@ const Profile = () => {
                       </defs>
                     </svg>
                   </Link>
+
                   <Link
-                    href="#"
-                    className="hover:text-primary"
-                    aria-label="social-icon"
-                  >
-                    <svg
-                      className="fill-current"
-                      width="23"
-                      height="22"
-                      viewBox="0 0 23 22"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g clipPath="url(#clip0_30_970)">
-                        <path
-                          d="M20.9813 5.18472C20.2815 5.49427 19.5393 5.69757 18.7795 5.78789C19.5804 5.30887 20.1798 4.55498 20.4661 3.66672C19.7145 4.11405 18.8904 4.42755 18.0315 4.59714C17.4545 3.97984 16.6898 3.57044 15.8562 3.43259C15.0225 3.29474 14.1667 3.43617 13.4218 3.83489C12.6768 4.2336 12.0845 4.86726 11.7368 5.63736C11.3891 6.40746 11.3056 7.27085 11.4993 8.0933C9.97497 8.0169 8.48376 7.62078 7.12247 6.93066C5.76118 6.24054 4.56024 5.27185 3.59762 4.08747C3.25689 4.67272 3.07783 5.33801 3.07879 6.01522C3.07879 7.34439 3.75529 8.51864 4.78379 9.20614C4.17513 9.18697 3.57987 9.0226 3.04762 8.72672V8.77439C3.04781 9.65961 3.35413 10.5175 3.91465 11.2027C4.47517 11.8878 5.2554 12.3581 6.12304 12.5336C5.55802 12.6868 4.96557 12.7093 4.39054 12.5996C4.63517 13.3616 5.11196 14.028 5.75417 14.5055C6.39637 14.983 7.17182 15.2477 7.97196 15.2626C7.17673 15.8871 6.2662 16.3488 5.29243 16.6212C4.31866 16.8936 3.30074 16.9714 2.29688 16.8502C4.04926 17.9772 6.08921 18.5755 8.17271 18.5735C15.2246 18.5735 19.081 12.7316 19.081 7.66522C19.081 7.50022 19.0765 7.33339 19.0691 7.17022C19.8197 6.62771 20.4676 5.95566 20.9822 5.18564L20.9813 5.18472Z"
-                          fill=""
-                        />
-                      </g>
-                      <defs>
-                        <clipPath id="clip0_30_970">
-                          <rect
-                            width="22"
-                            height="22"
-                            fill="white"
-                            transform="translate(0.666138)"
-                          />
-                        </clipPath>
-                      </defs>
-                    </svg>
-                  </Link>
-                  <Link
-                    href="#"
+                    href={authContext?.user?.socials?.linkedin || "#"}
+                    target="_blank"
                     className="hover:text-primary"
                     aria-label="social-icon"
                   >
@@ -255,7 +218,8 @@ const Profile = () => {
                     </svg>
                   </Link>
                   <Link
-                    href="#"
+                    href={authContext?.user?.socials?.behance || "#"}
+                    target="_blank"
                     className="hover:text-primary"
                     aria-label="social-icon"
                   >
@@ -281,7 +245,8 @@ const Profile = () => {
                     </svg>
                   </Link>
                   <Link
-                    href="#"
+                    href={authContext?.user?.socials?.github || "#"}
+                    target="_blank"
                     className="hover:text-primary"
                     aria-label="social-icon"
                   >
